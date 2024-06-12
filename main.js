2a9e1a57c1110d85b907a8fc8a4331482ee0032e
Dmitriy <dmitriy@MacBook-Pro.local>
2024-06-12 18:57:57 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import {Telegraf, Markup} from 'telegraf'
+
+const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
+const url = 'https://react-test-tg-bot.web.app/'
+const bot = new Telegraf(token, {})
+
+bot.command('start', ctx => {
+    ctx.reply('Welcome to Telegraf1!',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('send message', url)
+        ]))
+})
+
+bot.launch()
