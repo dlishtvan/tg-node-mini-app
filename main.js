fe6c09596084076fc9bee8adec7c2ca723e25d00
Dmytro <dmitriylishtvan@gmail.com>
2024-06-13 18:23:45 +0300

    switching to the node-telegram-bot-api

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-import {Telegraf, Markup} from 'telegraf'
+import TelegramBot from 'node-telegram-bot-api'
 import express from "express";
 import cors from "cors";
 
 const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
 const url = 'https://react-test-tg-bot.web.app/'
-const bot = new Telegraf(token, {})
+const bot = new TelegramBot(token, {polling: true});
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-bot.command('start', ctx => {
-    ctx.reply('Welcome to Telegraf5!',
-        Markup.inlineKeyboard([
-            Markup.button.webApp('send message', url)
-        ]))
-})
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-bot.launch()
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'qq', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: url + '/form'}}]
+                ]
+            }
+        })
+    }
+});
 
 const PORT = 8000;
-app.listen(PORT, () => `Server running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
