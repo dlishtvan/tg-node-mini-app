cb6596c4190c95b0a0f464e3329e60fca782e015
Dmytro <dmitriylishtvan@gmail.com>
2024-06-13 16:46:32 +0300

    test

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-import {Telegraf, Markup} from 'telegraf'
-
-const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
-const url = 'https://react-test-tg-bot.web.app/'
-const bot = new Telegraf(token, {})
-
-bot.command('start', ctx => {
-    ctx.reply('Welcome to Telegraf222!',
-        Markup.inlineKeyboard([
-            Markup.button.webApp('send message', url)
-        ]))
-})
-
-bot.launch()
+// import {Telegraf, Markup} from 'telegraf'
+//
+// const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
+// const url = 'https://react-test-tg-bot.web.app/'
+// const bot = new Telegraf(token, {})
+//
+// bot.command('start', ctx => {
+//     ctx.reply('Welcome to Telegraf222!',
+//         Markup.inlineKeyboard([
+//             Markup.button.webApp('send message', url)
+//         ]))
+// })
+//
+// bot.launch()
+console.log("test")
