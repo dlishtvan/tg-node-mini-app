807d8acdf6730a8929f2a8eb863768b9972dd590
Dmytro <dmitriylishtvan@gmail.com>
2024-06-13 17:00:08 +0300

    test express

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-// import {Telegraf, Markup} from 'telegraf'
-//
-// const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
-// const url = 'https://react-test-tg-bot.web.app/'
-// const bot = new Telegraf(token, {})
-//
-// bot.command('start', ctx => {
-//     ctx.reply('Welcome to Telegraf222!',
-//         Markup.inlineKeyboard([
-//             Markup.button.webApp('send message', url)
-//         ]))
-// })
-//
-// bot.launch()
-console.log("test")
+import {Telegraf, Markup} from 'telegraf'
+import express from "express";
+import cors from "cors";
+
+const token = '7418620026:AAHHDANp6hg6iREZ-vtiPUiDld9Lb_X0c9c'
+const url = 'https://react-test-tg-bot.web.app/'
+const bot = new Telegraf(token, {})
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+bot.command('start', ctx => {
+    ctx.reply('Welcome to Telegraf222!',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('send message', url)
+        ]))
+})
+
+bot.launch()
+
+const PORT = 8000;
+app.listen(PORT, () => `Server running on port ${PORT}`);
